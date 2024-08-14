7846f7c17eb8f769375af0149b0114ea4ca7da2d
sangjin <sangjincha719@gmail.com>
2024-08-15 00:16:20 +0900

    댓글 알림

--- comment-router.js
+++ comment-router.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 //다른 파일
-const {formatDate } = require("./index");
+const {formatDate, sendNotification } = require("./index");
 const { pool } = require("./database");
 
 
@@ -15,25 +15,92 @@ router.post("/set", (req, res) => {
   let id = uuidv4();
   let created_at = formatDate(false, "");
   let content = req.body.content;
-  let user_id = req.body.user_id;
-  let image_id = req.body.image_id;
+  let user_id = req.body.user_id; //댓글 작성하는 유저
+  let image_id = req.body.image_id; //작성되는 이미지 아이디
 
 
-  let query = `insert into infixel_db.comments values ('${id}','${content}', '${user_id}', '${image_id}', '${created_at}');`;
+  let insertQuery = `insert into infixel_db.comments values ('${id}','${content}', '${user_id}', '${image_id}', '${created_at}');`;
+  let getUserQuery = `
+    select c.contents, u.id, u.device_token 
+      from 
+    infixel_db.comments as c 
+      join 
+    infixel_db.images as i 
+      on c.image_id = i.id
+      join
+    infixel_db.users as u
+      on u.id = i.user_id 
+      where i.id = '${image_id}'
+      ;
+    `
   
   pool.getConnection((err, connection) => {
     if (err) {
-      return res.status(500).json({ error: "MySQL 연결 실패" });
+      return res.status(500).json({ error : "MySQL 연결 실패."})
     }
 
-    connection.query(query, (queryErr, results) => {
-      connection.release();
-      if (queryErr) {
-        return res.status(500).json({ result: false });
+    connection.beginTransaction((err) => {
+      if (err) {
+        connection.release();
+        return res.status(500).json({ error : "트랜잭션 시작 실패."})
       }
-      return res.json({ result: true });
-    });
-  });
+
+      connection.query(getUserQuery, (queryErr, results) => {
+        if (queryErr) {
+          return connection.rollback(() => {
+            connection.release();
+            return res.status(500).json({result: false})
+          })
+        }
+
+        let device_token = results[0].device_token;
+        sendNotification(device_token, `회원님의 사진에 '${content}'댓글이 작성되었습니다.`)
+
+        connection.query(insertQuery, (queryErr) => {
+          if (queryErr) {
+            return connection.rollback(() => {
+              connection.release();
+              return res.status(500).json({ result : false})
+            })
+          }
+
+          connection.commit((err) => {
+            if (err) {
+              return connection.rollback(() => {
+                connection.release();
+                return res.status(500).json({ error: "트랜잭션 커밋 실패."})
+              })
+            }
+            connection.release();
+            return res.json({ result: true})
+          })
+
+
+        })
+
+
+      })
+
+    })
+
+
+
+  })
+
+
+  // pool.getConnection((err, connection) => {
+  //   if (err) {
+  //     return res.status(500).json({ error: "MySQL 연결 실패" });
+  //   }
+
+  //   connection.query(insertQuery, (queryErr, results) => {
+  //     connection.release();
+  //     if (queryErr) {
+  //       return res.status(500).json({ result: false });
+  //     }
+  //     return res.json({ result: true });
+  //   });
+  // });
 });
 //============================================================
 
